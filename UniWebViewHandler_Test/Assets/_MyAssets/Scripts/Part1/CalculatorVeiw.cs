3f74da181582dc97c1805cd5da25d24ed4ea1987
Aviv5374 <aviv5374@gmail.com>
2020-10-07 10:48:14 +0300

    calculator 1 (Veiw1)

---
+++ UniWebViewHandler_Test/Assets/_MyAssets/Scripts/Part1/CalculatorVeiw.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CalculatorVeiw : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI calculatorScreen;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTabb(GameObject buttenTMP)
+    {
+        string newTXT = buttenTMP.GetComponent<TextMeshProUGUI>().text;
+        calculatorScreen.text += newTXT;
+    }
+
+}
