176b66e9b84a7f75fb770f15305569ee8463d1f6
Aviv5374 <aviv5374@gmail.com>
2020-10-07 09:13:19 +0300

    First Unity Setup 2

---
+++ UniWebViewHandler_Test/Assets/_MyAssets/Scripts/SceneLoader.cs
@@ -0,0 +1,157 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    //the build index of the scene which the game realy start with.
+    //scenes like: Main Menu, level1, ...
+    const int firstBuildSceneIndex = 0;
+
+    const int firstGameSceneIndex = 0;
+
+    [SerializeField] float defaultWaitingTime = 3.5f;
+
+    int LastBuildSceneIndex
+    {
+        get
+        {
+            return SceneManager.sceneCountInBuildSettings - 1;
+        }
+    }
+
+    Scene CurrentScene
+    {
+        get
+        {
+            return SceneManager.GetActiveScene();
+        }
+    }
+
+    Scene firstSceneThisGameObjectWasLoaded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        firstSceneThisGameObjectWasLoaded = CurrentScene;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    #region Basic Functionality
+
+    public void FirstScene()
+    {
+        SceneManager.LoadScene(firstBuildSceneIndex);
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(firstGameSceneIndex);
+    }
+
+    public void NextScene()
+    {
+        int nextScenebuildIndex = CurrentScene.buildIndex + 1;
+        if (nextScenebuildIndex < SceneManager.sceneCountInBuildSettings)
+        {
+            SceneManager.LoadScene(nextScenebuildIndex);
+        }
+        else
+        {
+            //TODO: decision what the game loop is, or fined another resolution!!
+        }
+    }
+
+    public void ResetScene()
+    {
+        SceneManager.LoadScene(CurrentScene.buildIndex);
+    }
+
+    public void PreviousScene()
+    {
+        SceneManager.LoadScene(CurrentScene.buildIndex - 1);
+    }
+
+    public void LastScene()
+    {
+        SceneManager.LoadScene(LastBuildSceneIndex);
+    }
+
+    #endregion
+
+    #region Advance Functionality
+
+    //ToDo: Write more overloads to this methods 
+    public void WaitAndLoadScene(string sceneToLoad, float timeToWait = 0)
+    {
+        StartCoroutine(WaitAndLoadSceneRoutine(sceneToLoad, timeToWait));
+    }
+
+    private IEnumerator WaitAndLoadSceneRoutine(string sceneToLoad, float givenWaitingTime = 0)
+    {
+        //Wait
+        if (givenWaitingTime > 0)
+        {
+            yield return new WaitForSeconds(givenWaitingTime);
+        }
+        else
+        {
+            yield return new WaitForSeconds(defaultWaitingTime);
+        }
+
+        //Load Scene
+        LoadByKey(sceneToLoad);
+
+    }
+
+    #endregion
+
+    #region Spesifice Functionality
+
+    public void LoadByIndex(int sceneToLoad)
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+
+    public void LoadByName(string sceneToLoad)
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+
+    public void LoadByKey(string sceneToLoad)
+    {
+        switch (sceneToLoad)
+        {
+            case "First":
+                FirstScene();
+                break;
+            case "RestartGame":
+                RestartGame();
+                break;
+            case "Next":
+                NextScene();
+                break;
+            case "Reset":
+                ResetScene();
+                break;
+            case "Previous":
+                PreviousScene();
+                break;
+            case "Last":
+                LastScene();
+                break;
+            default:
+                Debug.LogError("Wrong Load Action Key / Scene Name To Load!!!!!");
+                break;
+        }
+    }
+
+    #endregion
+
+}
---
+++ UniWebViewHandler_Test/Assets/_MyAssets/Scripts/Singleton.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Singleton<T> : MonoBehaviour where T : Component
+{
+    private static T instance = null;
+
+    //gameObject existent in the scene -> the gameObject is active -> 
+    //a singleton script is a component on the gameObject -> Awake() is executed ->
+    //Singelton Pattern -> instance != null
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                //!!!!!!!!!!!
+                //TODO: read this
+                //what happen if FindObjectsOfType<T>().Length >= 1 ?
+                //the intention is that script B call Singleton.Instance after script B Awake()(meaning in Start()), 
+                //so the Awake() in the manager(or script A) is executed forst thus the Singleton Pattern will work properly.
+                //but what if, for some stupid reason, script B call Singleton.Instance in it's Awake(), 
+                //and script B Awake() is executed before script A Awake() ?
+                //probably is -> FindObjectsOfType<T>().Length >= 1!!! the quetion is how I deal with it ?
+                //!!!!!!!!!
+                GameObject obj = new GameObject(typeof(T).Name);
+                obj.AddComponent<T>();
+                //!!!!!!!!!
+                //ToDo: Do I need to buy time between "obj.AddComponent<T>();" to "return instance;"
+                //to make sure Awake() actived and instance != null ?
+                //in my STRONGE computer the code work, but will it work on weaker computers? 
+                //!!!!!!!!!!!
+                Debug.LogWarning("A new " + typeof(T).Name + " as been created!!!!");
+            }
+
+            return instance;
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        #region Singelton Pattern
+        //Check if instance already exists
+        if (instance == null)
+        {
+            //Set instance to this as T
+            instance = this as T;
+            //Set this gameObject to not be destroyed when reloading scene
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            //Make sure this GameObject is out from the life cycle of Unity Engine.
+            gameObject.SetActive(false);
+            //And destroy this gameObject to enforces the singleton pattern.             
+            Destroy(gameObject);
+        }
+        #endregion
+
+    }
+
+
+}
